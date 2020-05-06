51d4892eecdc1e76655a5b6e2149d043b84a3b78
yn07 <yn07@LAPTOP-M0IMMK01>
2020-05-06 14:30:18 +0900

    jps Dispatcher, Redirect 방식

---
+++ HTML5/src/com/test/jsp/DispatcherServlet.java
@@ -0,0 +1,32 @@
+package com.test.jsp;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/dispatcherServlet")
+public class DispatcherServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+	
+	private void doProc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		request.setAttribute("data", "데이터");
+		RequestDispatcher rd = request.getRequestDispatcher("redirect.jsp");
+		rd.forward(request, response);
+	}
+
+}
