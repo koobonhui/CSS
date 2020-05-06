c6582f5d69254c992db7271274d6cf5bd85cd7dd
yn07 <yn07@LAPTOP-M0IMMK01>
2020-05-06 14:29:19 +0900

    jsp 로그인

---
+++ HTML5/src/com/test/jsp/SessionLoginServlet.java
@@ -0,0 +1,45 @@
+package com.test.jsp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/sessionLogin")
+public class SessionLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+	
+	protected void doProc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String id = request.getParameter("id");
+		String passwd = request.getParameter("passwd");
+		if(id.equals("java") && passwd.equals("java")) {
+			HttpSession session = request.getSession();
+			session.setAttribute("id", id);
+			RequestDispatcher rd = request.getRequestDispatcher("menu.jsp");
+			rd.forward(request, response);
+		} else {
+			out.println("<script>");
+			out.println("alert('아이디나 비밀번호 불일치');");
+			out.println("history.back();");
+			out.println("</script>");
+		}
+	}
+
+}
