fa3d1d116b8838121526cceadc9e4db1de1251da
yn07 <yn07@LAPTOP-M0IMMK01>
2020-05-21 15:50:07 +0900

    웹디자인기능사-공개도면-A-1 (Action -> Servlet)

---
+++ HTML5/src/com/test/ex/servlet/LogoutServlet.java
@@ -0,0 +1,35 @@
+package com.test.ex.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/logoutAction.do")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+	
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		PrintWriter out = response.getWriter();
+		
+		session.invalidate();
+		out.println("<script>");
+		out.println("location.href = 'index10.jsp'");
+		out.println("</script>");
+	}
+}
